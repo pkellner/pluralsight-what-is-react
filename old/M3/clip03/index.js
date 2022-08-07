9dd5e407f9dd98b6f5bc9143d09db38939d7ba1b
pkellner <peter@peterkellner.net>
2022-08-07 08:10:39 -0700

    App that is used in course to demonstrate todo. It has extra animations as well as code to highlight components with orange borders.

--- old/M3/clip03/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-  const rootElement = document.getElementById("root");
-  const button = document.createElement("button");
-  button.innerHTML = "Click me for current date";
-  button.addEventListener("click", () => {
-    button.innerHTML = new Date().toString();
-  });
-  rootElement.appendChild(button);
-};
--- old/M3/clip05/myapp/pages/index copy.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      Hello From Pluralsight
-    </div>
-  )
-}
