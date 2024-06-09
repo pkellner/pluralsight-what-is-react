1fa569cda073ff205d5ab0056d182d60d9bbed32
pkellner <peter@peterkellner.net>
2024-06-09 07:53:55 -0700

    both client-only and combined RSC and CO working in 3-weather-b

---
+++ m4-how-react-works-with-the-browser/clip-99i/app/3-weather-b-client-only/load-data.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+export default function LoadData({ children }) {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Hide Data" : "Load Data"}
+      </button>
+      {show && <>{children}</>}
+    </>
+  );
+}
