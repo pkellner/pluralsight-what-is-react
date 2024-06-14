f4818f383a303894244807d0add4458b06d816fa
pkellner <peter@peterkellner.net>
2024-06-14 15:04:40 -0700

    update

--- m3-react-architecture-explained/clip-09/myapp/src/app/page.js
+++ m3-react-architecture-explained/clip-09/myapp/src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState } from "react";
 export default function Home() {
   function ListItems({ints, addValue}) {
     const increment = 3;
