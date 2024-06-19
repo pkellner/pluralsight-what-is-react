7e644807c9f6a75dda12098094d7fb87216832f7
pkellner <peter@peterkellner.net>
2024-06-19 11:24:35 -0700

    update

--- m3-react-architecture-explained/clip-04/index.js
+++ m3-react-architecture-explained/clip-04/index.js
@@ -1,11 +1,12 @@
 window.onload = () => {
   const rootElement = document.getElementById("root");
+  const root = ReactDOM.createRoot(rootElement);
 
   const ints = [1, 2, 3];
 
-  ints.forEach(i => {
-    let li = document.createElement("li");
-    li.innerHTML = i;
-    rootElement.appendChild(li);
-  })
+  // this refactor is not in the clip, but would be the obvious next step
+  const childrenElements = ints.map((id, index) => {
+    return React.createElement("li", { key: ints[index] }, ints[index]);
+  });
+  root.render(childrenElements);
 };
